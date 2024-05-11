0a6be5f0e4abdce3a5829a4eb8c67221874e99aa
Shambhu-Rep <shambhusah1115@gmail.com>
2024-05-11 14:28:58 +0530

    Git checked

---
+++ src/main/java/com/test/GitSample.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class GitSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git1");
+		System.out.println("Git1");
+	}
+
+}
